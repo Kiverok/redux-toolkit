5faf33aec0a01d931ca78a0c6ede871adfa5d35a
Kiverok <kiver566@ukr.net>
2023-01-04 20:33:04 +0200

    provider

--- src/App.js
+++ src/App.js
@@ -4,49 +4,32 @@ import TodoList from './components/TodoList';
 import InputField from './components/InputField';
 
 function App() {
-  const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
 
-const addTodo = () => {
-  if (text.trim().length) {
-    setTodos([
-      ...todos,
-      {
-        id: new Date().toISOString(),
-        text,
-        completed: false,
-      }
-    ])
-    setText('');
-  }
-}
 
 const toggleTodoComplete = (todoId) => {
-  setTodos(
-    todos.map(
-      todo => {
-        if (todo.id !== todoId) return todo;
+//   setTodos(
+//     todos.map(
+//       todo => {
+//         if (todo.id !== todoId) return todo;
 
-       return {
-        ...todo,
-        completed: !todo.completed,
-       }
-      }
-    )
-  )
-}
+//        return {
+//         ...todo,
+//         completed: !todo.completed,
+//        }
+//       }
+//     )
+//   )
+// }
 
 const removeTodo = (todoId) => {
-  setTodos(todos.filter(todo => todo.id !== todoId))
-}
+//   setTodos(todos.filter(todo => todo.id !== todoId))
+// }
 
   return (
     <div className="App">
       <InputField text={text} handleInput={setText} handleSubmit={addTodo} />
-      <TodoList todos={todos} 
-      toggleTodoComplete={toggleTodoComplete}
-      removeTodo={removeTodo}
-       />
+      <TodoList />
     </div>
   );
 }
